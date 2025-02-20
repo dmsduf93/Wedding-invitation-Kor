af8518892c38afcb0202b49af04398db5718e8c1
EunYeol Kim <68711879+dmsduf93@users.noreply.github.com>
2025-02-20 12:05:28 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,6 +16,29 @@ $(document).bind("contextmenu", function (e) {
       $('.sakura-falling').sakura();
 })(jQuery);
 
+// Swipper toggle
+var swiper = new Swiper('.swiper-container', {
+  loop: true, 
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
+
 
 // Parallax 효과
 const images = document.getElementsByClassName('couple-drawing');
